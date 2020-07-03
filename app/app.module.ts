2a884f5a799c6f05256932731e950e1492f24042
michelelaporta <michele.laporta@gmail.com>
2020-07-03 14:53:55 +0200

    2 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,7 +10,7 @@ import {VgCoreModule} from 'videogular2/core';
 import {VgControlsModule} from 'videogular2/controls';
 import {VgOverlayPlayModule} from 'videogular2/overlay-play';
 import {VgBufferingModule} from 'videogular2/buffering';
-import {SingleMediaPlayer} from './single-media-player';
+// import {SingleMediaPlayer} from './single-media-player';
 
 @NgModule({
   imports:      [ 
